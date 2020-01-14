c4ce48a5567434a60d4ffb36b2f316dab8263a5e
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2020-01-14 21:40:24 +0100

    Imagenes donete

--- Proyecto-DI-Mediawolf-API/common/models/image.js
+++ Proyecto-DI-Mediawolf-API/common/models/image.js
@@ -1,35 +1,16 @@
 'use strict';
 
 module.exports = function(Image) {
-
-    /*Image.beforeRemote('upload',function(ctx,next){
-        let fs = require("fs");
-        console.log('jor',ctx.req.name)
-        fs.rename('./files/images/'+ctx.args.formData.name,'./files/images/'+ctx.args.name,function(err) {
-            if ( err ) console.log('ERROR: ' + err);
-        });
-        next();
-    });*/
-
-
-   /* Image.Upload = function(img,name,cb){
-        let image = img
-
-    // luego extraes la cabecera del data url
-    var base64Data = image.data.replace(/^data:image\/jpeg;base64,/, "");
-
-    // grabas la imagen el disco
-    fs.writeFile(name+'.png', base64Data, 'base64', function(err) {
-        console.log(err);
-    });
-    cb(null,'Imagen insertada');
+   const fs = require('fs');
+    Image.FileUpload = function(file,name, cb){
+        
+        fs.writeFile('./files/images/'+name,file,'base64',function (err) {});
+        cb(null,'ALGO'+file);
     }
 
-    Image.remoteMethod('upload', {
-        accepts: {arg: 'img',type:''},
-        accepts:{arg:'name',type:'string'}
-  });*/
-
-    
-
+    Image.remoteMethod('FileUpload', {
+        accepts:[ {arg: 'file', type: 'string'},
+                {arg: 'name', type:'string'}],
+        returns: {arg: 'resp', type: 'string'}
+  });
 };
